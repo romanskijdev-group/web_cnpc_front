edfd8853fc383f081cced5f77bf5c1d0f6409a6c
Pavel <p.volokh.a@gmail.com>
2024-09-17 15:12:40 +0300

    Update UserAgreement.tsx
    one more update user agree page (cry smile)

--- src/pages/UserAgreement.tsx
+++ src/pages/UserAgreement.tsx
@@ -1,6 +1,6 @@
 const UserAgreement = () => {
     return(
-        <div className="container mx-auto ">
+        <div className="sm:max-w-screen-xl w-[20%] mx-auto mt-[150px] border dark:border-gray-500 rounded-lg p-4 container mx-auto ">
             <h1>Пользовательское соглашение</h1>
                 <div className="">
                     <div className='flex flex-col gap-[10px]'>
@@ -9,7 +9,7 @@ const UserAgreement = () => {
                     <a href='https://google.com/'
                        className='dark:text-gray-200 hover:text-gray-500 dark:hover:text-gray-400 duration-300'>Политика конфеденциальности</a>
                     <a href='https://google.com/'
-                       className='dark:text-gray-200 hover:text-gray-500 dark:hover:text-gray-400 duration-300'>Возможности</a>
+                       className='dark:text-gray-200 hover:text-gray-500 dark:hover:text-gray-400 duration-300'>Политика безопасности</a>
                     </div>
 
                 </div>
