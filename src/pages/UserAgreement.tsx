4ab77633fcd6a99f2b88b143499ca571504a04cd
Pavel <p.volokh.a@gmail.com>
2024-09-17 09:43:39 +0300

    Update UserAgreement.tsx

--- src/pages/UserAgreement.tsx
+++ src/pages/UserAgreement.tsx
@@ -1,7 +1,18 @@
 const UserAgreement = () => {
     return(
-        <div>
+        <div className="container mx-auto ">
             <h1>Пользовательское соглашение</h1>
+                <div className="">
+                    <div className='flex flex-col gap-[10px]'>
+                    <a href='https://google.com/'
+                       className='dark:text-gray-200 hover:text-gray-500 dark:hover:text-gray-400 duration-300'>Финансовая политика</a>
+                    <a href='https://google.com/'
+                       className='dark:text-gray-200 hover:text-gray-500 dark:hover:text-gray-400 duration-300'>Политика конфеденциальности</a>
+                    <a href='https://google.com/'
+                       className='dark:text-gray-200 hover:text-gray-500 dark:hover:text-gray-400 duration-300'>Возможности</a>
+                    </div>
+
+                </div>
         </div>
     )
 }
