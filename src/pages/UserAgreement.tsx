26a7720dbb7bf44dedce1c6db2f12102499a3a99
Pavel <p.volokh.a@gmail.com>
2024-09-23 19:27:05 +0300

    Update UserAgreement.tsx

--- src/pages/UserAgreement.tsx
+++ src/pages/UserAgreement.tsx
@@ -1,20 +1,30 @@
 const UserAgreement = () => {
     return(
-        <div className="sm:max-w-screen-xl w-[20%] mx-auto mt-[150px] border dark:border-gray-500 rounded-lg p-4 container mx-auto ">
-            <h1>Пользовательское соглашение</h1>
-                <div className="">
-                    <div className='flex flex-col gap-[10px]'>
-                    <a href='https://google.com/'
-                       className='dark:text-gray-200 hover:text-gray-500 dark:hover:text-gray-400 duration-300'>Финансовая политика</a>
-                    <a href='https://google.com/'
-                       className='dark:text-gray-200 hover:text-gray-500 dark:hover:text-gray-400 duration-300'>Политика конфеденциальности</a>
-                    <a href='https://google.com/'
-                       className='dark:text-gray-200 hover:text-gray-500 dark:hover:text-gray-400 duration-300'>Политика безопасности</a>
-                    </div>
-
+        <div className="flex w-[90%] h-[100vh] mx-auto">
+            <div className="w-64"> 
+                <div className="p-4">
+                    <h1 className="text-xl text-gray-800 dark:text-white mb-4 ">Пользовательское соглашение</h1>
                 </div>
+                
+                <nav className="flex flex-col gap-4 px-4 py-2">
+                    <a href="https://google.com/" className="text-gray-600 hover:text-gray-800 dark:hover:text-gray-200 duration-300"> Финансовая политика </a>
+                    <a href="https://google.com/" className="text-gray-600 hover:text-gray-800 dark:hover:text-gray-200 duration-300"> Политика конфеденциальности </a>
+                    <a href="https://google.com/" className="text-gray-600 hover:text-gray-800 dark:hover:text-gray-200 duration-300"> Политика безопасности
+                    </a>
+                </nav>
+            </div>
+        
+            <div className="flex-1 p-4 ">
+                <div className="sm:max-w-screen-xl border dark:border-gray-500 rounded-lg p-4 container">
+                    <div className="prose dark:prose-invert">
+                        <p> Текст</p>
+                    </div>
+                </div>
+            </div>
         </div>
+
     )
 }
 
 export default UserAgreement
+
