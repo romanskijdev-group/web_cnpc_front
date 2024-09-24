8c97e90161f86ee143c623161c27cfc3f28f62f4
Pavel <p.volokh.a@gmail.com>
2024-09-24 16:47:14 +0300

    Update UserAgreement.tsx

--- src/pages/UserAgreement.tsx
+++ src/pages/UserAgreement.tsx
@@ -1,27 +1,29 @@
 const UserAgreement = () => {
     return(
-        <div className="flex w-[90%] h-[100vh] mx-auto">
-            <div className="w-64"> 
-                <div className="p-4">
-                    <h1 className="text-xl text-gray-800 dark:text-white mb-4 ">Пользовательское соглашение</h1>
+        <main className="flex w-[100vw] h-[100vh] mx-auto items-center">
+            <div className="flex w-[90%] h-[100vh] mx-auto">
+                <div className="w-64"> 
+                    <div className="w-screen p-4">
+                        <h1 className="text-xl text-gray-800 dark:text-white mb-4">Пользовательское соглашение</h1>
+                    </div>
+                    
+                    <nav className="flex flex-col gap-4 px-4 py-2">
+                        <a href="https://google.com/" className="text-gray-600 hover:text-gray-800 dark:hover:text-gray-200 duration-300"> Финансовая политика </a>
+                        <a href="https://google.com/" className="text-gray-600 hover:text-gray-800 dark:hover:text-gray-200 duration-300"> Политика конфеденциальности </a>
+                        <a href="https://google.com/" className="text-gray-600 hover:text-gray-800 dark:hover:text-gray-200 duration-300"> Политика безопасности
+                        </a>
+                    </nav>
                 </div>
-                
-                <nav className="flex flex-col gap-4 px-4 py-2">
-                    <a href="https://google.com/" className="text-gray-600 hover:text-gray-800 dark:hover:text-gray-200 duration-300"> Финансовая политика </a>
-                    <a href="https://google.com/" className="text-gray-600 hover:text-gray-800 dark:hover:text-gray-200 duration-300"> Политика конфеденциальности </a>
-                    <a href="https://google.com/" className="text-gray-600 hover:text-gray-800 dark:hover:text-gray-200 duration-300"> Политика безопасности
-                    </a>
-                </nav>
-            </div>
-        
-            <div className="flex-1 p-4 ">
-                <div className="sm:max-w-screen-xl border dark:border-gray-500 rounded-lg p-4 container">
-                    <div className="prose dark:prose-invert">
-                        <p> Текст</p>
+            
+                <div className="flex-1 p-4 ">
+                    <div className="sm:max-w-screen-xl border dark:border-gray-500 rounded-lg p-4 container">
+                        <div className="prose dark:prose-invert">
+                            <p>Текст</p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </main>
 
     )
 }
