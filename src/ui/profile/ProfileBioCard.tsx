13c4b729ff3a9b223a33087f668358cd1946e6f6
Pavel <p.volokh.a@gmail.com>
2024-10-21 13:13:04 +0300

    created profile description block

---
+++ src/ui/profile/ProfileBioCard.tsx
@@ -0,0 +1,9 @@
+export const ProfileBioCard = () => {
+    return (
+    <div className='container w-3/5 ml-48 py-16 rounded-lg bg-gray-600'>
+        <div className="">
+            <p>Привет! Меня зовут Роман, я создатель сервера EldirtchMagic и по совместительству разработчик данной платформы. Будем знакомы!</p>
+        </div>
+    </div>
+    )
+}
