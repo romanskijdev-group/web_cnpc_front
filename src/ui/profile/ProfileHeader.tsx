f63658ce8bdafcbc8d894a9af7592ebca6d45fa1
Pavel <p.volokh.a@gmail.com>
2024-10-21 13:13:35 +0300

    update developer name
    ananasananasananas

--- src/ui/profile/ProfileHeader.tsx
+++ src/ui/profile/ProfileHeader.tsx
@@ -3,7 +3,7 @@ import { ProfileAvatar } from './ProfileAvatar'
 export const ProfileHeader = () => {
     return(
         <div className='w-full bg-gradient-to-br from-yellow-500 via-red-500 to-pink-500 text-white shadow-md h-[15vh] rounded-lg relative'>
-            <ProfileAvatar name='Sansara'/>
+            <ProfileAvatar name='Ananas'/>
         </div>
     )
 }
