e0897d2089b94ba64aef4d5504af15b456e4fd34
Roman <banan12.github@senthy.com>
2025-04-09 22:11:52 +0300

    Update ProfileHeader.tsx

--- src/ui/profile/ProfileHeader.tsx
+++ src/ui/profile/ProfileHeader.tsx
@@ -1,11 +1,8 @@
 import React from "react";
-import { CustomThemeSettings } from "../background/CustomThemeSettings";
 
 export const ProfileHeader: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    const { divStyle } = CustomThemeSettings();
-
     return (
-        <div style={divStyle} className='col-start-1 col-span-4 md:col-start-2 md:col-span-2 border dark:border-[#27282D] shadow-md rounded-lg relative bg-white dark:bg-[#1B1C22]'>
+        <div className='col-start-1 col-span-4 md:col-start-2 md:col-span-2 border dark:border-[#27282D] shadow-md rounded-lg relative bg-white dark:bg-[#1B1C22]'>
             {children}
         </div>
     )
